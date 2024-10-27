30f930e7768bcc5407b688a8bc53cdeeeb1299f9
AdrianVizc <vizcara1@uci.edu>
2024-10-26 21:02:17 -0700

    got rid of delay

--- Fall2024Jam/Assets/Scripts/Buttons.cs
+++ Fall2024Jam/Assets/Scripts/Buttons.cs
@@ -6,35 +6,23 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     [SerializeField] private AudioClip buttonSFX;
-    [SerializeField] private float delayBeforeSceneLoad = 1f;
+
 
     public void PlayButton()
     {
         SoundFXManager.instance.PlaySFX(buttonSFX, transform, 1f);
-        StartCoroutine(LoadSceneAfterDelay("LevelScene"));
+        SceneManager.LoadScene("LevelScene");
     }
 
     public void MainMenuButton()
     {
         SoundFXManager.instance.PlaySFX(buttonSFX, transform, 1f);
-        StartCoroutine(LoadSceneAfterDelay("MainMenuScene"));
+        SceneManager.LoadScene("MainMenuScene");
     }
 
     public void QuitButton()
     {
         SoundFXManager.instance.PlaySFX(buttonSFX, transform, 1f);
-        StartCoroutine(QuitAfterDelay());
-    }
-
-    IEnumerator LoadSceneAfterDelay(string sceneName)
-    {
-        yield return new WaitForSeconds(delayBeforeSceneLoad);
-        SceneManager.LoadScene(sceneName);
-    }
-
-    IEnumerator QuitAfterDelay()
-    {
-        yield return new WaitForSeconds(delayBeforeSceneLoad);
         Application.Quit();
         Debug.Log("Quit");
     }
