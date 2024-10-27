0e5010fd3f7b7421f5b8b499438dcf8209d5e428
Nicholas Lee <nklee2@uci.edu>
2024-10-27 00:54:31 -0700

    Added Mirror Image + TestLevelScene

---
+++ Fall2024Jam/Assets/MirrorController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorController : MonoBehaviour
+{
+    [SerializeField] GameObject fullMirror;
+    [SerializeField] GameObject slightlyCrackedMirror;
+    [SerializeField] GameObject veryCrackedMirror;
+    [SerializeField] GameObject brokenMirror;
+
+
+}
