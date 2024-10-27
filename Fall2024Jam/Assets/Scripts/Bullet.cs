2e836b877226f59b4b4410aec1cca1845909fdf7
dominic-tran <dominic-tran@users.noreply.github.com>
2024-10-26 18:36:58 -0700

    Revert "MainMenu Player"
    This reverts commit 79e609b77baf01b0ec27913183d331ce9e2e48d7.

--- Fall2024Jam/Assets/Scripts/Bullet.cs
+++ Fall2024Jam/Assets/Scripts/Bullet.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float timer;
-    private void Update()
-    {
-        timer += Time.deltaTime;
-        Delete();        
-    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Enemy")
@@ -17,13 +11,6 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
         if (collider.tag == "Wall")
-        {
-            Destroy(gameObject);
-        }
-    }
-    private void Delete()
-    {
-        if (timer > 5)
         {
             Destroy(gameObject);
         }
