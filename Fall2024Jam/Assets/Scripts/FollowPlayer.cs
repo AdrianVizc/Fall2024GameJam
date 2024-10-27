490faa847562fced24e40c64359a3ee083c4fb8f
dominic-tran <dominic-tran@users.noreply.github.com>
2024-10-26 18:10:11 -0700

    NavMesh Added

--- Fall2024Jam/Assets/Scripts/FollowPlayer.cs
+++ Fall2024Jam/Assets/Scripts/FollowPlayer.cs
@@ -1,23 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowPlayer: MonoBehaviour
 {
     private Transform target;
+    private NavMeshAgent agent;
 
-    [SerializeField]
-    private float speed;
-
-    // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        agent.SetDestination(target.position);
     }
 }
